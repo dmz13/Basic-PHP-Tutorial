a6ab52a45bee9e3d2e14989a17aec760dc5b59a9
Michael <michael@dmz13.com>
2012-01-16 11:35:54 +1000

    added first php file

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Hello World</title>
+	</head>
+	<body>
+	<?php 
+	  	// single-line comments are like this
+	  	# or like this
+		/* our double like comment here
+		and we can keep typing
+		and typing
+		*/
+	echo "Hello World!<br />"; ?>
+	<?php print "Hello World!"; ?><br />
+	<?php echo "Hello"." World!"; ?><br />
+	<?php echo 2+3; ?><br />
+	</body>
+</html>
